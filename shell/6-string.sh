868ae7398bff200b0a00a347b379452a2c6ae6c3
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-09-05 12:04:03 +0530

    ss

--- shell/6-string.sh
+++ shell/6-string.sh
@@ -3,14 +3,23 @@ read str
 
 echo ${#str}    # this will return the length of str
 
+newStr=""
 
 for ((i=0; i<=${#str}; i++))
 do
 	#echo "${str:i:1}"   # char at i the index. This will print the char from 
 	#echo "${str:i:2}"   # i to i+2 char
-	char="${str:$i:1}"
-	char="a"
-	str="${str}${char}"
+	# char="${str:$i:1}"
+	# char="a"
+	# str="${str}${char}"
+
+	remaing=$((${#str}-i))
+	char="${str:remaing:1}"
+
+	newStr="$newStr$char"
+
 done 
 
-echo $str
+# echo $str
+
+echo $newStr
